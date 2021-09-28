d8863b18add991e5170358280da6d2a48ada422a
Suzan Dsouza <89693202+suzandsouza@users.noreply.github.com>
2021-09-28 19:52:16 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,47 +1,51 @@
-#include <stdio.h>
-#include <graphics.h>
-#include <math.h>
-#pragma GCC diagnostic ignored "-Wwrite-strings"
-//#define ROUND(x) (int)(x+0.5)
-main()
+//Implementation of Bresenham line drawing algorithm
+#include <stdlib.h>
+#include<math.h>
+void lineBres(int x0,int y0,int xEnd,int yEnd)
 {
-     int gd = DETECT,gm = DETECT;
-     float x1,y1,x2,y2,x,y;
-     printf("\nEnter two end points of a line: ");
-     printf("\nEnter x1,y1::");
-     scanf("%f %f",&x1,&y1);
-     printf("\nEnter x2,y2:: ");
-     scanf("%f %f",&x2,&y2);
-     initgraph(&gd,&gm,"");
-     x=x1;
-     y=y1;
-     putpixel(round(x),round(y),WHITE);
-     int dx=x2-x1;
-     int dy=y2-y1;
-     int steps;
-     if(abs(dx)>abs(dy))
-        steps=dx;
-     else
-        steps=dy;
-     float xinc=(float)dx/steps;
-     float yinc=(float)dy/steps;
-     for(int k=1;k<=steps;k++)
-     {
-         x=x+xinc;
-         y=y+yinc;
-
-
-     putpixel(round(x),round(y),WHITE);
-     }
-     getch();
-
-
+    int dx=fabs(xEnd-x0), dy=fabs(yEnd-y0);
+    int p=2*dy-dx;
+    int twoDy=2*dy, twoDyMinusdx=2*(dy-dx);
+    int x,y;
+    if(x0>xEnd)
+    {
+        x=xEnd;
+        y=yEnd;
+        xEnd=x0;
+    }
+    else{
+        x=x0;
+        y=y0;
+    }
+    putPixel(x,y);
+    while(x<xEnd)
+    {
+        x++;
+        if(p<0)
+            p+=twoDy;
+        else
+        {
+            y++;
+            p=+twoDyMinusdx;
+        }
+        putPixel(x,y);
+    }
 }
 
 
+int main()
+{
+    int gd=DETECT,gm=DETECT;
+    int x0,y0,xEnd,yEnd;
 
+    printf("Enter the coordiantes x1,y1");
+    scanf("%d%d",&x0,&y0);
 
+    printf("Enter the coordiantes x2,y2");
+    scanf("%d%d",&xEnd,&yEnd);
 
+    initgraph(&gd,&gm," ");
+    lineBres(x0,y0,xEnd,yEnd);
+    return 0;
 
-
-
+}
